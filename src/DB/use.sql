338430539b379e7f5236b7952c88b4bf19f2768a
rikuto <rikuriku.shin@gmail.com>
2023-11-02 12:44:08 +0900

    テーブル作成sql追加

--- src/DB/use.sql
+++ src/DB/use.sql
@@ -39,3 +39,34 @@ CREATE TABLE Cart(
     foreign key (user_id) references User(user_id),
     foreign key (merchandise_id) references Merchandise(merchandise_id)
 );
+
+-- ポイントテーブル作成sql
+CREATE TABLE Point(
+    point_id int not null AUTO_INCREMENT,
+    user_id int not null,
+    point_use INT not null,
+    point_grant int not null,
+    point_date DATE not null,
+    primary key (point_id),
+    foreign key (user_id) references User(user_id)
+);
+
+-- 購入テーブル作成sql
+CREATE TABLE Purchase(
+    purchase_id int not null AUTO_INCREMENT,
+    user_id int not null,
+    cart_id int not null,
+    purpose_date DATE not null,
+    primary key (purchase_id)
+);
+
+-- 購入詳細テーブル作成sql
+CREATE TABLE PurchaseDet(
+    details_id int not null AUTO_INCREMENT,
+    purchase_id int not null,
+    merchandise_id int not null,
+    quantity INT not null,
+    primary key (details_id),
+    foreign key (purchase_id) references Purchase(purchase_id),
+    foreign key (merchandise_id) Merchandise(merchandise_id)
+);
