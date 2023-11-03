21343472449aed52dad3a33272e7a946f1dfb3ad
rikuto <rikuriku.shin@gmail.com>
2023-11-04 01:18:59 +0900

    参考用sample(G1-4-1)追加

---
+++ src/G1-4/G1-4-1/sample.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="../imge/cart.png">
+        <img src="../imge/people.jpg">
+        <p>ロゴ</p>
+        <h2>カテゴリ</h2>
+        <div>
+            <input type="checkbox" id="suitu" name="suitu" />
+            <label for="suitu">洋菓子・スイーツ</label>
+        </div>
+        <div>
+            <input type="checkbox" id="wagasi" name="wagasi" />
+            <label for="wagasi">和菓子</label>
+        </div>
+        <div>
+            <input type="checkbox" id="otumami" name="otumami" />
+            <label for="otumami">おつまみ</label>
+        </div>
+
+        <h2>価格</h2>
+        <div>
+            <input type="checkbox" id="sen" name="sen" />
+            <label for="sen">～1000円</label>
+        </div>
+        <div>
+            <input type="checkbox" id="seniti" name="seniti" />
+            <label for="seniti">1001～2000円</label>
+        </div>
+        <div>
+            <input type="checkbox" id="senni" name="senni" />
+            <label for="senni">2001円～3000円</label>
+        </div>
+        <div>
+            <input type="checkbox" id="sensan" name="sensan" />
+            <label for="sensan">3001円～4000円</label>
+        </div>
+        <div>
+            <input type="checkbox" id="senyon" name="senyon" />
+            <label for="senyon">4001～5000円</label>
+        </div>
+        <div>
+            <input type="checkbox" id="gosen" name="gosen" />
+            <label for="gosen">5001～円</label>
+        </div>
+    </header>
+</body>
+</html>
