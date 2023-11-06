46420fc6c2a0f4a068c96bd09d25b678c7fd8bd2
rikuto <rikuriku.shin@gmail.com>
2023-11-06 13:42:16 +0900

    共通部品ファイル構築

---
+++ src/common/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- 共通header.cssと繋げる -->
+</head>
+<body>
+    <header id="header">
+        <!-- header内の必要要素を入れる -->
+
+        <!-- 遷移を可能にしておく -->
+
+    </header>
+</body>
+</html>
