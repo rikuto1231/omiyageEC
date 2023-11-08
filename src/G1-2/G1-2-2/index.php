e13b3ce0e91c67f0551fa9a759cb8d7b40f43c88
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-11-09 00:34:28 +0900

    aa

--- src/G1-2/G1-2-2/index.php
+++ src/G1-2/G1-2-2/index.php
@@ -3,17 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/G1-2-2.css"/>
     <title>Document</title>
 </head>
 <body>
     <h1>会員ログイン</h1>
-    <p>メールアドレス
+    <p>メールアドレス<br>
         <input type="text" name="mail" placeholder="入力してください">
     </p>
-    <p>パスワード
+    <p>パスワード<br>
         <input type="text" name="password" placeholder="パスワードを入力">
     </p>
-    <a href="" class="btn" onclick="location.href='G1-2-2/index.php'">ログイン</a>
-    <a href="" class="btn2" onclick="location.href='../G1-2-3/index.php'">戻る</a>
+    <p><a href="" class="btn" onclick="location.href='G1-2-2/index.php'">ログイン</a></p>
+    <p><a href="" class="btn2" onclick="location.href='../G1-2-3/index.php'">戻る</a></p>
 </body>
 </html>
