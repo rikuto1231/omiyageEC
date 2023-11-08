d538f66fc96931c835790c95d5753cfd2f0ec650
Hibio117 <2201181@s.asojuku.ac.jp>
2023-11-08 12:23:36 +0900

    a

---
+++ src/G1-3/G1-3-1/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>マイページ画面</title>
+</head>
+<body>
+    <a href=""><img src="../imge/user.png"width="40" height="40"></a>
+
+    <header>
+        <div class="sidebar">
+            <img id="closeButton" src="../imge/close.png">
+            <!-- サイドバーの内容 -->
+            <img src="../imge/user.png"width="40" height="40">
+            <p>○○○○さん</p>
+            <hr>
+            <p><a href="">ポイント</a></p>
+            <hr>
+            <p><a href="">登録情報</a></p>
+            <hr>
+            <p><a href="">購入履歴</a></p>
+            <hr>
+            <p><a href="">予約確認</a></p>
+            <hr>
+            <p><a href="">ログイン</a></p>
+            <hr>
+            <p><a href="">ログアウト</a></p>
+            <hr>
+        </div>
+        <img id="toggleButton"src="../imge/user.png"width="40" height="40">
+        <a href=""><img src="../imge/cart.jpg" width="40" height="40"></a>
+    </header>
+    <script src="side_ber.js"></script>
+</body>
+</html>
