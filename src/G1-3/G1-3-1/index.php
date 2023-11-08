fd5c4e82c660f8ad67fe583f0661fc7987e88050
Hibio117 <2201181@s.asojuku.ac.jp>
2023-11-08 11:55:29 +0900

    a

--- src/G1-3/G1-3-1/index.php
+++ src/G1-3/G1-3-1/index.php
@@ -8,6 +8,34 @@
 <body>
     <a href=""><img src="../imge/user.png"width="40" height="40"></a>
 
+<header>
+        <div class="sidebar">
+            <img id="closeButton" src="img/close.png">
+            <!-- サイドバーの内容 -->
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+        </div>
+        <img id="toggleButton" src="img/menu.png">
+        <a href="task_name.html"><img id="listButton" src="img/list.png"></a>
+        <a href="task_name.html"><img id="addButton" src="img/add.png"></a>
+        
+    </header>
+    <script src="side_ber.js"></script>
+    
+</body>
+</html>
     
 </body>
 </html>
