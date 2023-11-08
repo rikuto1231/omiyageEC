c38f88307224382d26b0a859fb0913a80858ad83
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-08 12:06:03 +0900

    h

--- src/G1-5/G1-5-1/index.php
+++ src/G1-5/G1-5-1/index.php
@@ -3,7 +3,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>カート画面</title>
 </head>
