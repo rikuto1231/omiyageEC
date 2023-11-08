477e8611cd365e410508b8f52724ddffe32432fe
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-11-08 11:52:25 +0900

    yy

--- src/G1-2/G1-2-1/index.php
+++ src/G1-2/G1-2-1/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/G1-2-1.css"/>
+    <link rel="stylesheet" href="css/G1-2-1.css"/>
     <title>Document</title>
 </head>
 
@@ -16,7 +16,7 @@
     <!-- </form> -->
 
 
-        <img><img src="image/mement.png" alt="めめんと"></img>
+       <img src="image/mement.png" alt="めめんと"></img>
     <p>初めての方はこちらから<br>
         <button onclick="location.href='../G1-2-3/index.php'">新規会員登録</button>
     </p>
