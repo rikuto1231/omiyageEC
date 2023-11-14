1eff05481abf48cec378a6acc831904ea77625f1
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-14 11:44:35 +0900

    s

--- src/G1-5/G1-5-2/index.php
+++ src/G1-5/G1-5-2/index.php
@@ -46,6 +46,15 @@
         <div class="product">
             <p id="cart_mess">カートに入っているアイテム</p>
             <img class="img_product" src="../imge/cart.jpg" width="100" height="100">
+            <span>○○○○○○</span>
+            <br>
+            <select name="number">
+                <?php
+                for($i = 1; $1 <= 10; $i++) {
+                    echo '<option value="', $i, '">', $i, '</option>';
+                }
+                ?>
+                </select>
             <button class="button_next">ショッピングを続ける</button>
         </div>
         <div id="num_output">
