080d91338316e8eb7ded1409bbc401e99eaf0880
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-11-14 12:01:07 +0900

    lll

--- src/G2-2-1/index.php
+++ src/G2-2-1/index.php
@@ -1,11 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php session_start(); ?>
+
+<?php
+$name=$address=$login=$password='';
+if(isset($_SESSION['customer'])){
+    $name=$_SESSION['customer']['name'];
+    $address=$_SESSION['customer']['address'];
+    $login=$_SESSION['customer']['login'];
+    $password=$_SESSION['customer']['password'];
+}
+echo '<form action="customer-output.php" method="post">';
+echo '<table>';
+echo '<tr><td>お名前</td><td>';
+echo '<input type="text" name="name" value="',$name,'">';
+echo '</td></tr>';
+echo '<tr><td>ご住所</td><td>';
+echo '<input type="text" name="address" value="',$address,'">';
+echo '</td></tr>';
+echo '<tr><td>ログイン名</td><td>';
+echo '<input type="text" name="login" value="',$login,'">';
+echo '</td></tr>';
+echo '<tr><td>パスワード</td><td>';
+echo '<input type="password" name="password" value="',$password,'">';
+echo '</td></tr>';
+echo '</table>';
+echo '<input type="submit" value="確定">';
+echo '</form>';
+?>
+<?php require 'footer.php';?>
+
