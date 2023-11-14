61be95f79010543dd3a92c3a72bb288d85ab7d9b
kanato0407 <148302672+kanato0407@users.noreply.github.com>
2023-11-14 12:14:13 +0900

    変更しました

--- src/G1-4/G1-4-3/index.php
+++ src/G1-4/G1-4-3/index.php
@@ -15,7 +15,8 @@
                     <div class="container">
                         <h3>ロゴ・検索部分（後々php読み込みで追加）</h3>
                     </div>
-                    
+                <h3>〇〇県</h3>
+                <h3>〇〇〇〇〇〇</h3>
             </header>
         </body>
         </html>
