07c98ecbecba0b87e16331e6e47f7282ecaeaaa8
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-11-14 14:57:31 +0900

    ｋｋ

--- src/G2-1/G2-2-1/index.php
+++ src/G2-1/G2-2-1/index.php
@@ -9,27 +9,29 @@
 <body>
     <p>商品追加</p>
     <img src="img/icon1.png" alt="アイコン１"></img>
-    
+    <table class="table" border="1" cellpadding="10"cellspacing="0">
     <form action="G2-2-1/index.php" method="post">
-    <table>
+
     <tr><td>名称指定</td><td>
-    <input type="text" name="name" >
+    <input type="text" size="30" class="name" >
      </td></tr>
      <tr><td>都道府県</td><td>
-     <input type="text" name="prefecture">
+     <input type="text" size="30" class="prefecture">
     </td></tr>
      <tr><td>カテゴリ</td><td>
-    <input type="text" name="cate" >
+    <input type="text" size="30" class="cate" >
     </td></tr>
     <tr><td>価格</td><td>
-    <input type="number" name="price" >
+    <input type="number" class="price" >
     </td></tr>
     <tr><td>画像</td><td>
     <input type="file" name="img" >
     </td></tr>
     </table>
-    <input type="submit" value="追加">
-    </form>
-
+    
+    
+    <p><a href="" class="add" onclick="location.href='G1-1/index.php'">追加</a></p>
+</form>
+    
 </body>
 </html>
