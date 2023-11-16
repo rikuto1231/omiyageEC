82fe71ae1ea4fa1d36516b7fc00aeeaee5b3bb53
rikuto <rikuriku.shin@gmail.com>
2023-11-16 12:26:51 +0900

    パス通し調整完成

--- src/common/php/header.php
+++ src/common/php/header.php
@@ -2,7 +2,7 @@
 
 <?php
     echo '<div class="sidebar">
-        <img id="closeButton" src="../img/close.png">
+        <img id="closeButton" src="img/close.png">
 
         <hr>
         <p>○○○○さん</p>
@@ -19,14 +19,14 @@
         <hr>
         <p><a href="">ログアウト</a></p>
         </div>
-        <img id="toggleButton" src="/img/user.png" width="40" height="40">
+        <img id="toggleButton" src="img/user.png" width="40" height="40">
         <a href="">
         <img id="cart_button" src="img/cart.jpg" width="40" height="40">
         </a>
         <div id="search_all">
         <input type="text" class="search_box" placeholder="検索">
         <button class="conditions_img">
-            <img src="../img/kensakku.png" width="20" height="20">
+            <img src="img/kensakku.png" width="20" height="20">
         </button>
         <p><a href="" id="conditions">絞り込み条件を開く▽</a></p>
         </div>';
