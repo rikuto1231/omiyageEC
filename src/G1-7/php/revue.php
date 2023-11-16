7d0e450d9a9515d0fb65d907a2013ed37d2fa9e2
2201150 <147681633+2201150@users.noreply.github.com>
2023-11-16 12:31:26 +0900

    更新

---
+++ src/G1-7/php/revue.php
@@ -0,0 +1,17 @@
+    <div id="center">
+    <input type="text"class="search">
+    </div>
+    <br>
+    <div id="revew">
+    <h2><img src="../../G1-1/img/user.png" alt="代替テキスト"width="7%" height="14%">
+    あいうえお
+    <br>
+    m/d
+    <br>
+    ★☆☆☆☆
+    <br>
+    とてもいい</h2>
+    <div id="center">
+    <div id="fonts">×月×日に購入<br>~~~~~~~~~~<br>~~~~~~~~~~<br>~~~~~~~~~~</div>
+    </div>
+    </div>
