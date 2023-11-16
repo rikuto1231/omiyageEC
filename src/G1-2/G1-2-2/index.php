a76afe7bd00da2aa2b50ef299ecd4fea8cc1f26f
rikuto <rikuriku.shin@gmail.com>
2023-11-16 10:32:28 +0900

    修正必要箇所です

--- src/G1-2/G1-2-2/index.php
+++ src/G1-2/G1-2-2/index.php
@@ -8,14 +8,21 @@
 </head>
 <body>
     <h1>会員ログイン</h1>
+    <form method="post" action="../G1-2-3/index.php">
     <p><div id="mail">メールアドレス</div><br>
-        <div class="box1"><input type="text" class="m" placeholder="入力してください" ></div>
+        <div class="box1">
+            <input type="text" class="m" placeholder="入力してください" required>
+        </div>
     </p>
     <p><div id="pass" >パスワード</div><br>
-        <div class="box2"><input type="text" class="p" placeholder="パスワードを入力" ></div>
+        <div class="box2">
+            <input type="text" class="p" placeholder="パスワードを入力" required>
+        </div>
     </p>
-        <!-- 別phpの関数呼び出しで入力チェックとlocation -->
+
+    <!-- ログインは送信タイプのボタンに変更 -->
     <p><a href="" class="btn" onclick="location.href='G1-1/index.php'">ログイン</a></p>
+    
     <p><a href="" class="btn2" onclick="location.href='../G1-2-1/index.php'">戻る</a></p>
 </body>
 </html>
