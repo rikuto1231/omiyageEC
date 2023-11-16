8480672a4b7c04c5b63cc12c494a51508f543a28
rikuto <rikuriku.shin@gmail.com>
2023-11-16 10:50:00 +0900

    遷移できるように修正

--- src/G1-2/G1-2-2/index.php
+++ src/G1-2/G1-2-2/index.php
@@ -9,24 +9,23 @@
 <body>
     <h1>会員ログイン</h1>
     <form method="post" action="../G1-2-3/index.php">
-    <p><div id="mail">メールアドレス</div><br>
-        <div class="box1">
-            <input type="text" class="m" placeholder="入力してください" required>
-        </div>
-    </p>
-    <p><div id="pass" >パスワード</div><br>
-        <div class="box2">
-            <input type="text" class="p" placeholder="パスワードを入力" required>
-        </div>
-    </p>
 
-    <!-- ログインは送信タイプのボタンに変更 -->
-    <p><a href="" class="btn" onclick="location.href='G1-1/index.php'">ログイン</a></p>
+        <p>
+            <label for="mail">メールアドレス</label><br>
+            <div class="box1">
+                <input type="text" id="mail" name="mail" class="m" placeholder="入力してください" required>
+            </div>
+        </p>
+        <p>
+            <label for="pass">パスワード</label><br>
+            <div class="box2">
+                <input type="password" id="pass" name="pass" class="p" placeholder="パスワードを入力" required>
+            </div>
+        </p>
 
+        <button type="submit" class="btn">ログイン</button>
     </form>
 
-    <p><a href="" class="btn2" onclick="location.href='../G1-2-1/index.php'">戻る</a></p>
-
-    
+    <button class="btn2" onclick="location.href='../G1-2-1/index.php'">戻る</button>
 </body>
 </html>
