c79b5571c5a1ec9524102bebe7f89d8f5879c285
Hibio117 <2201181@s.asojuku.ac.jp>
2023-11-17 12:33:43 +0900

    a

---
+++ src/G2-3/G2-3-1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>検索・全件表示画面（更新）</title>
+</head>
+<body>
+    
+</body>
+</html>
