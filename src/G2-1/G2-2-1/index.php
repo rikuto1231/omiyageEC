1fa82231b2b9d577967c5b669c338581f7fbd92c
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-11-17 12:04:49 +0900

    ぉぉ

--- src/G2-1/G2-2-1/index.php
+++ src/G2-1/G2-2-1/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/G2-2-1.css">
+    <link rel="stylesheet" href="css/G2-2-1.css">
     <title>商品追加</title>
 </head>
 <body>
