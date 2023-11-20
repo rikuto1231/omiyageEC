5f544d213512b55b0d2787a505845511c8c4c5d2
2201150 <147681633+2201150@users.noreply.github.com>
2023-11-20 12:18:12 +0900

    更新

---
+++ src/G1-2/G1-2-5/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="G1-2-5/style.css">
+    <title>ログイン登録確定画面</title>
+</head>
+<body>
+<form method="post" action="../G1-2-6/index.php" id="form_all">
+    <!-- 入力データを表示 -->
+    
+    <div id="container">
+        
+    </div>
+    <!-- 確認ボタン -->
+    <div id="button_control">
+        <button type="submit" class="btn-check">登録確定する</button><br>
+    </div>
+
+    <!-- 戻るボタン -->
+    <div id="button_control">
+        <button type="button" onclick="history.back()" class="btn-back">編集する</button>
+    </div>
+</body>
+</html>
