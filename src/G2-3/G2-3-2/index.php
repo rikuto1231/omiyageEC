9eef650928523aeec92ee066c8ec50b9747d0425
Hibio117 <2201181@s.asojuku.ac.jp>
2023-11-20 12:01:01 +0900

    a

--- src/G2-3/G2-3-2/index.php
+++ src/G2-3/G2-3-2/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>検索・全件表示画面（更新）</title>
+    <title>検索結果画面（更新）</title>
 </head>
 <body>
 <div class="ber">
