78f48a92aa324a1362ddf16d676861fee7da91a5
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-21 11:58:10 +0900

    a

--- src/G1-1/index.php
+++ src/G1-1/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vue Image Slider</title>
+    <title>トップ画面</title>
     <link rel="stylesheet" href="../common/css/header.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
