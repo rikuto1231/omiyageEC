440392a5fdc01deb23cc94a513f9aa0467e69cd4
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-21 14:18:29 +0900

    f

--- src/G1-4/G1-4-2/index.php
+++ src/G1-4/G1-4-2/index.php
@@ -1,48 +1,48 @@
 <!DOCTYPE html>
-<!-- 参考
-    https://zero-plus.io/media/flex-wrap-how-to-use/ -->
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-
+    <title>検索後の商品詳細画面</title>
 </head>
-
 <body>
+    <header>
+        <?php require '../common/php/header.php'; ?>
+    </header>
+    <div class="main">
     <div class="flexbox">
         <div class="item">
-            <img src="../img/aso.jpg" alt="代替テキスト" width="90%" height="30%">
+            <img src="../img/aso.jpg" alt="代替テキスト" width="90px" height="90px">
             <p>商品名</p>
         </div>
         <div class="item">
-            <img src="../img/aso.jpg" alt="代替テキスト" width="90%" height="30%">
+            <img src="../img/aso.jpg" alt="代替テキスト" width="90px" height="90px">
             <p>商品名</p>
         </div>
         <div class="item">
-            <img src="../img/aso.jpg" alt="代替テキスト" width="90%" height="30%">
+            <img src="../img/aso.jpg" alt="代替テキスト" width="90px" height="90px">
             <p>商品名</p>
         </div>
         <div class="item">
-            <img src="../img/aso.jpg" alt="代替テキスト" width="90%" height="30%">
+            <img src="../img/aso.jpg" alt="代替テキスト" width="90px" height="90px">
             <p>商品名</p>
         </div>
         <div class="item">
-            <img src="../img/aso.jpg" alt="代替テキスト" width="90%" height="30%">
+            <img src="../img/aso.jpg" alt="代替テキスト" width="90px" height="90px">
             <p>商品名</p>
         </div>
         <div class="item">
-            <img src="../img/aso.jpg" alt="代替テキスト" width="90%" height="30%">
+            <img src="../img/aso.jpg" alt="代替テキスト" width="90px" height="90px">
             <p>商品名</p>
         </div>
-
     </div>
-
+    </div>
     <div class ="button1">
     <button class="kakunin">検索ホームに戻る</button>
     </div>
+    <script src="../common/js/side_ber.js"></script>
 </body>
 
 </html>
