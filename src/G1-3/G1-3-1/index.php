9b4e8b75f74a1ea30ac6b006c0c1771c0e624eb2
Hibio117 <2201181@s.asojuku.ac.jp>
2023-11-22 11:33:07 +0900

    a

--- src/G1-3/G1-3-1/index.php
+++ src/G1-3/G1-3-1/index.php
@@ -11,7 +11,7 @@
         <div class="sidebar">
             <img id="closeButton" src="../imge/close.png">
             <!-- サイドバーの内容 -->
-            <img src="../img/user.png"width="40" height="40">
+            <img src="../imge/user.png"width="40" height="40">
             <p>○○○○さん</p>
             <hr>
             <p><a href="">ポイント</a></p>
@@ -27,8 +27,8 @@
             <p><a href="">ログアウト</a></p>
             <hr>
         </div>
-        <img id="toggleButton"src="../img/user.png"width="40" height="40">
-        <a href=""><img src="../img/cart.jpg" width="40" height="40"></a>
+        <img id="toggleButton"src="../imge/user.png"width="40" height="40">
+        <a href=""><img src="../imge/cart.jpg" width="40" height="40"></a>
     </header>
     <script src="side_ber.js"></script>
 
