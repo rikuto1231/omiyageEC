2839e006c9ac90994cbd5ef58be521d2bae7d332
rikuto <rikuriku.shin@gmail.com>
2023-11-22 12:03:15 +0900

    DB.phpファイルへの接続を追加

--- src/G1-4/G1-4-2/index.php
+++ src/G1-4/G1-4-2/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../../common/css/header.css">
+    <?php require '../../common/php/DB.php'; ?>
     <title>検索後の商品詳細画面</title>
 </head>
 <body>
