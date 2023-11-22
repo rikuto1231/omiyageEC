d42b0e6f8156adcde2dbf0a4551d82a531f7c413
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-22 12:27:25 +0900

    h

--- src/common/php/header.php
+++ src/common/php/header.php
@@ -7,11 +7,11 @@
         <hr>
         <p>○○○○さん</p>
         <hr>
-        <p><a href="">ポイント</a></p>
+        <p><a href="../../G1-3/G1-3-4/index.php">ポイント</a></p>
         <hr>
-        <p><a href="">登録情報</a></p>
+        <p><a href="../../G1-3/G1-3-2/index.php">登録情報</a></p>
         <hr>
-        <p><a href="">購入履歴</a></p>
+        <p><a href="../../G1-3/G1-3-3/index.php">購入履歴</a></p>
         <hr>
         <p><a href="">予約確認</a></p>
         <hr>
