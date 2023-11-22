31dffe66f627dd1aa3e99bed8a40aabd3fb051e4
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-11-22 11:42:54 +0900

    fff

--- src/common/php/logout.php
+++ src/common/php/logout.php
@@ -1,4 +1,12 @@
 <?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP Sample Programs</title>
+</head>
+<body>
+
 
 <?php
     if(isset($_SESSION['user_id'])){
