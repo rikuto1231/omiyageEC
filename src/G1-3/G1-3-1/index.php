ddccf0a0ae9bd4172aa56277d403de33033c6697
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-22 11:56:39 +0900

    hu

--- src/G1-3/G1-3-1/index.php
+++ src/G1-3/G1-3-1/index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../common/css/header.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>マイページ画面</title>
 </head>
 <body>
-<header>
         <?php require '../common/php/header.php'; ?>
-</header>
+        <script src="../common/js/side_ber.js"></script>
 </body>
 </html>
