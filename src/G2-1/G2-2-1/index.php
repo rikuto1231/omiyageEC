7dd0dfcdd40e4397e5512f73974aa39da872aaf3
rikuto <rikuriku.shin@gmail.com>
2023-11-27 11:28:00 +0900

    修正

---
+++ src/G2-1/G2-2-1/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品追加</title>
+    <link rel="stylesheet" href="css/G2-2-1.css"/>
+</head>
+<body>
+<div class="ber">
+    <img src="img/kanri_icon.png" alt="">
+        <p><img src="img/kanri_icon2.png" class="icon1">商品情報追加▶</p>
+        <p><img src="img/kanri_icon3.png" class="icon1">商品情報更新</p>
+        <p><img src="img/kanri_icon4.png" class="icon1">商品情報削除</p>
+    </div>
+    <p>商品追加</p>
+    
+    <div class="tab">
+    <table class="table" border="1" cellpadding="10"cellspacing="0">
+    <form action="G2-2-1/index.php" method="post">
+
+    <tr><td>名称指定</td><td>
+    <input type="text" size="30" class="name" >
+     </td></tr>
+     <tr><td>都道府県</td><td>
+     <input type="text" size="30" class="prefecture">
+    </td></tr>
+     <tr><td>カテゴリ</td><td>
+    <input type="text" size="30" class="cate" >
+    </td></tr>
+    <tr><td>価格</td><td>
+    <input type="number" class="price" >
+    </td></tr>
+    <tr><td>画像</td><td>
+    <input type="file" name="img" >
+    </td></tr>
+    </table>
+</div>
+    
+    
+   
+   <a href="" class="btn">追加</a>
+</form>
+    
+</body>
+</html>
---
+++ src/tmp/test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class="sidebar">
+            <img id="closeButton" src="img/close.png">
+            <!-- サイドバーの内容 -->
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+            <p>test1</p>
+            <hr>
+        </div>
+        <img id="toggleButton" src="img/menu.png">
+        <a href="task_name.html"><img id="listButton" src="img/list.png"></a>
+        <a href="task_name.html"><img id="addButton" src="img/add.png"></a>
+        
+    </header>
+    <script src="side_ber.js"></script>
+    
+</body>
+</html>
