b47d7e5648f5a031413eede9f7598760ff30381e
rikuto <rikuriku.shin@gmail.com>
2023-11-28 12:06:58 +0900

    検索ロジック修正

--- src/common/php/search.php
+++ src/common/php/search.php
@@ -1,11 +1,12 @@
 <?php
     require 'DB.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // GETリクエストが送信された場合の処理
 
     // 接続
     $pdo = getDatabaseConnection(); 
+    echo 'AAAAAAAA';
 
     // 情報がなかった時の代替え文字列
     $search = isset($_GET['search_box']) ? $_GET['search_box'] : '';
