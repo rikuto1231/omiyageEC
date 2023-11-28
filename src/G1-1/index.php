18af09b125db4421c9f52109d0e7b9b08c633465
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-11-28 14:48:32 +0900

    o

--- src/G1-1/index.php
+++ src/G1-1/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>トップ画面</title>
-    <link rel="stylesheet" href="../../common/css/header.css">
+    <link rel="stylesheet" href="../common/css/header.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -104,6 +104,6 @@
   </div>
 </div>
     </div>
-    <script src="../../common/js/side_ber.js"></script>
+    <script src="../common/js/side_ber.js"></script>
 </body>
 </html>
