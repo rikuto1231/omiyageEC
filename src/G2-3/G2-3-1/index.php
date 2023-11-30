1b91bd5222d480c122e4b8d35e93feb79a40aa5d
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-30 12:03:53 +0900

    j

--- src/G2-3/G2-3-1/index.php
+++ src/G2-3/G2-3-1/index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -16,9 +17,9 @@
     </div>
     <div class="main">
         <h2>条件指定</h2>
+        <form action="../G2-3/G2-3-2/index.php"></form>
         <table class="table" border="1" cellpadding="10"cellspacing="0">
         <form action="" method="post">
-
         <tr><td class="td1">名称指定</td><td class="td2">
         <input type="text" size="30" class="text_box" >
         </td></tr>
@@ -28,46 +29,30 @@
         <tr><td class="td1">カテゴリ</td><td class="td2">
         <input type="text" size="30" class="text_box" >
         </td ></tr>
+        <tr><td class="td1">ブランド</td><td class="td2">
+        <input type="text" size="30" class="text_box">
+        </td></tr>
         <tr><td class="td1">価格帯</td><td class="td2">
         <input type="number" class="text_box" >
+        </td></tr>
+        <tr><td class="td1">在庫数</td><td class="td2">
+        <input type="text" size="30" class="text_box">
         </td></tr>
         </table>
         <button class="sarch">検索</button>
         <h2>全件表示</h2>
-    <div class="flexbox">
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト"width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト"width="70%" height="70%"">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-    </div>
+        <?php
+        require '../../common/php/DB.php';
+        $pdo = getDatabaseConnection();
+        echo'<div class="flexbox">';
+    foreach($pdo->query('select * from Merchandise') as $row){
+        echo'<div class="item">';
+            echo'<img src="omiyageEC/src/common/img/'.$row['path'].'" alt="代替テキスト" width="70%" height="70%">';
+            echo'<p class="mei">',$row['merchandise_name'],'</p>';
+            echo'</div>';
+        }
+    echo'</div>';
+    ?>
 </div>
     
 </body>
