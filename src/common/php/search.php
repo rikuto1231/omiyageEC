77cd47803835ab2790dd44ff4d834ae819b3e4da
rikuto <rikuriku.shin@gmail.com>
2023-11-30 20:58:11 +0900

    パスの修正

--- src/common/php/search.php
+++ src/common/php/search.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         // || !file_exists('/omiyageEC/src/common/img/'.$row['path'])
         if ($row['path'] == null || $row['path'] == '' ) { //パスがnullの時と空白の時と有効じゃないとき
             echo '<a href="/omiyageEC/src/G1-5/G1-5-3/index.php?merchandise_id=' . $row['merchandise_id'] . '">';
-            echo '<img src="'.'/omiyageEC/src/common/img/'.'no.jpeg'.'" alt="代替テキスト" width="90px" height="90px"'.$row['merchandise_id'].'>'; 
+            echo '<img src="'.'/omiyageEC/src/common/img/no.jpeg'.'" width="90px" height="90px"'.$row['merchandise_id'].'>'; 
             echo '</a>';
             echo '<p>'.$row['merchandise_name'].'</p>';
             echo '</div>';
