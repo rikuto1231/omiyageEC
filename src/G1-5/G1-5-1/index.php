7914747a32411cc0ea697b64d06d19428b194dbf
rikuto <rikuriku.shin@gmail.com>
2023-11-30 10:59:42 +0900

    タグの構造を修正

--- src/G1-5/G1-5-1/index.php
+++ src/G1-5/G1-5-1/index.php
@@ -7,42 +7,43 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="app">
-        <header>
+    <header>
+        <div id="app">
+        
             
-        <div class="sidebar">
-            <img id="closeButton" src="/omiyageEC/src/common/img/close.png">
+            <div class="sidebar">
+                <img id="closeButton" src="/omiyageEC/src/common/img/close.png">
 
-            <hr>
-            <p>○○○○さん</p>
-            <hr>
-            <!-- 絶対パスに変更 -->
-            <p><a href="../../G1-3/G1-3-4/index.php">ポイント</a></p>
-            <hr>
-            <p><a href="../../G1-3/G1-3-2/index.php">登録情報</a></p>
-            <hr>
-            <p><a href="../../G1-3/G1-3-3/index.php">購入履歴</a></p>
-            <hr>
-            <p><a href="../../G1-3/G1-3-5/index.php">予約確認</a></p>
-            <hr>
-            <p><a href="">ログイン</a></p>
-            <hr>
-            <p><a href="../../common/php/logout.php">ログアウト</a></p>
-        </div>
+                <hr>
+                <p>○○○○さん</p>
+                <hr>
+                <!-- 絶対パスに変更 -->
+                <p><a href="../../G1-3/G1-3-4/index.php">ポイント</a></p>
+                <hr>
+                <p><a href="../../G1-3/G1-3-2/index.php">登録情報</a></p>
+                <hr>
+                <p><a href="../../G1-3/G1-3-3/index.php">購入履歴</a></p>
+                <hr>
+                <p><a href="../../G1-3/G1-3-5/index.php">予約確認</a></p>
+                <hr>
+                <p><a href="">ログイン</a></p>
+                <hr>
+                <p><a href="../../common/php/logout.php">ログアウト</a></p>
+            </div>
 
-        <img id="toggleButton" src="/omiyageEC/src/common/img/user.png" width="40" height="40">
-        <a href="../../G1-5/G1-5-2/index.php">
-            <img id="cart_button" src="/omiyageEC/src/common/img/cart.jpg" width="40" height="40">
-        </a>
+            <img id="toggleButton" src="/omiyageEC/src/common/img/user.png" width="40" height="40">
+            <a href="../../G1-5/G1-5-2/index.php">
+                <img id="cart_button" src="/omiyageEC/src/common/img/cart.jpg" width="40" height="40">
+            </a>
 
-        <div id="search_all">
-            <form method="get" action="/omiyageEC/src/G1-5/G1-5-2/index.php">
-                <input type="text" class="search_box" placeholder="検索" value="" name="search_box">
-                <button class="conditions_img">
-                    <img src="/omiyageEC/src/common/img/search.png" width="20" height="20">
-                </button>
-            </form>
-        </div>
+            <div id="search_all">
+                <form method="get" action="/omiyageEC/src/G1-5/G1-5-2/index.php">
+                    <input type="text" class="search_box" placeholder="検索" value="" name="search_box">
+                    <button class="conditions_img">
+                        <img src="/omiyageEC/src/common/img/search.png" width="20" height="20">
+                    </button>
+                </form>
+            </div>
 
             <div id="menu_all">
                 <div class="container">
@@ -68,10 +69,10 @@
                     </div>
                 </div>
             </div>
-        </header>
-        <script src="/omiyageEC/src/common/js/side_ber.js"></script>
-    </div>
-    
+        </div>
+    </header>
+
+    <script src="/omiyageEC/src/common/js/side_ber.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <script src="script.js"></script>
 </body>
