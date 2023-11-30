126d7ea307007c092a835b2c4f213f182e8991aa
rikuto <rikuriku.shin@gmail.com>
2023-11-30 22:18:37 +0900

    修正

--- src/G1-5/G1-5-1/index.php
+++ src/G1-5/G1-5-1/index.php
@@ -9,7 +9,6 @@
 <body>
     <header>
 
-    </header>
         <div id="app">
         
             
@@ -72,6 +71,9 @@
                 </div>
             </div>
         </div>
+
+    </header>
+        
     <script src="/omiyageEC/src/common/js/side_ber.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <script src="script.js"></script>
