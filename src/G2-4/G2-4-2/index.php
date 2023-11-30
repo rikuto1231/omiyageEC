670344e3fb589284b1aecad1674a751341939073
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-30 15:49:24 +0900

    f

--- src/G2-4/G2-4-2/index.php
+++ src/G2-4/G2-4-2/index.php
@@ -15,58 +15,51 @@
         <p><img src="../imge/kanri_icon4.png" class="icon1"><span>商品情報削除▶</p></span>
     </div>
     <div class="main">
-        <h2>条件指定</h2>
+    <h2>条件指定</h2>
         <table class="table" border="1" cellpadding="10"cellspacing="0">
-        <form action="" method="post">
-
-        <tr><td class="td1">名称指定</td><td class="td2">
-        <input type="text" size="30" class="text_box" >
-        </td></tr>
-        <tr><td class="td1">都道府県</td><td class="td2">
-        <input type="text" size="30" class="text_box">
-        </td></tr>
-        <tr><td class="td1">カテゴリ</td><td class="td2">
-        <input type="text" size="30" class="text_box" >
-        </td ></tr>
-        <tr><td class="td1">価格帯</td><td class="td2">
-        <input type="number" class="text_box" >
-        </td></tr>
-        </table>
-        <button class="sarch">検索</button>
-    <div class="flexbox">
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト"width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト"width="70%" height="70%"">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-        <div class="item">
-            <img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">
-            <p class="mei">商品名</p>
-        </div>
-    </div>
+<?php
+        echo'<h2>条件指定</h2>';
+        echo'<table class="table" border="1" cellpadding="10"cellspacing="0">';
+        echo'<tr><td class="td1">名称指定</td><td class="td2">';
+        echo'<input type="text" size="30" class="text_box" >';
+        echo'</td></tr>';
+        echo'<tr><td class="td1">都道府県</td><td class="td2">';
+        echo'<input type="text" size="30" class="text_box">';
+        echo'</td></tr>';
+        echo'<tr><td class="td1">カテゴリ</td><td class="td2">';
+        echo'<input type="text" size="30" class="text_box" >';
+        echo'</td ></tr>';
+        echo'<tr><td class="td1">ブランド</td><td class="td2">';
+        echo'<input type="text" size="30" class="text_box" >';
+        echo'</td ></tr>';
+        echo'<tr><td class="td1">価格帯</td><td class="td2">';
+        echo'<input type="number" class="text_box" >';
+        echo'</td></tr>';
+        echo'<tr><td class="td1">在庫数</td><td class="td2">';
+        echo'<input type="text" size="30" class="text_box" >';
+        echo'</td ></tr>';
+        echo'</table>';
+        echo'<form action="../G2-3-1/index.php">';
+        echo'<button class="sarch">戻る</button>';
+        echo'</form>';
+    echo'<div class="flexbox">';
+    echo'<form method="POST" name="a_form" action="../G2-3-3/index.php">';
+    require '../../common/php/DB.php';
+        $pdo = getDatabaseConnection();
+        $sql=$pdo->prepare('select * from Merchandise where merchandise_name=? or prefectures=? or category=? or brand=? or price=? or stock=?');
+        $sql->execute([$_POST['merchandise_name'],$_POST['prefectures'],$_POST['category'],$_POST['brand'],$_POST['price'],$_POST['stock']]);
+    foreach ($sql as $row) {
+        echo'<div class="item">';
+        echo '<input type="hidden" name="id" value="',$row['merchandise_id'],'">';
+        echo'<a onclick="document.a_form.submit();">';
+        echo'<img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">';
+        echo'<p class="mei">',$row['merchandise_name'],'</p>';
+        echo'</a>';
+        echo'</div>';
+    }
+    echo'</form>';
+    echo'</div>';
+    ?>
 </div>
     
 </body>
