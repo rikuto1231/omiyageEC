f2cb357804d2149b123183c409bb596310614364
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-11-30 16:12:23 +0900

    k

--- src/G2-4/G2-4-2/index.php
+++ src/G2-4/G2-4-2/index.php
@@ -15,8 +15,6 @@
         <p><img src="../imge/kanri_icon4.png" class="icon1"><span>商品情報削除▶</p></span>
     </div>
     <div class="main">
-    <h2>条件指定</h2>
-        <table class="table" border="1" cellpadding="10"cellspacing="0">
 <?php
         echo'<h2>条件指定</h2>';
         echo'<table class="table" border="1" cellpadding="10"cellspacing="0">';
@@ -51,10 +49,8 @@
     foreach ($sql as $row) {
         echo'<div class="item">';
         echo '<input type="hidden" name="id" value="',$row['merchandise_id'],'">';
-        echo'<a onclick="document.a_form.submit();">';
         echo'<img src="../imge/a.png" alt="代替テキスト" width="70%" height="70%">';
         echo'<p class="mei">',$row['merchandise_name'],'</p>';
-        echo'</a>';
         echo'</div>';
     }
     echo'</form>';
