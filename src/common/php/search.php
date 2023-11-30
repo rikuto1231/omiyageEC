3da9ad268db63d0e11e9b5506e76c61696dbe9d4
rikuto <rikuriku.shin@gmail.com>
2023-11-30 12:16:33 +0900

    修正

--- src/common/php/search.php
+++ src/common/php/search.php
@@ -16,7 +16,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     foreach ($result as $row) {
         echo '<div class="item">';
         // 商品詳細用の特定ロジックを後で構築
-        if ($row['path'] == null || $row['path'] == '' || !file_exists('/omiyageEC/src/common/img/'.$row['path'])) { //パスがnullの時と空白の時と有効じゃないとき
+        // || !file_exists('/omiyageEC/src/common/img/'.$row['path'])
+        if ($row['path'] == null || $row['path'] == '' ) { //パスがnullの時と空白の時と有効じゃないとき
             echo '<img src="'.'/omiyageEC/src/common/img/'.'NO.jpeg'.'" alt="代替テキスト" width="90px" height="90px"'.$row['merchandise_id'].'>'; 
             echo !file_exists('/omiyageEC/src/common/img/'.$row['path']);
             echo '<p>'.$row['merchandise_name'].'</p>';
