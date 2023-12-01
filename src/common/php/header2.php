57a498cb56d0058108fd0a773a07669ce1f480ec
rikuto <rikuriku.shin@gmail.com>
2023-12-01 10:14:07 +0900

    登録情報出力用のphp適応

---
+++ src/common/php/header2.php
@@ -0,0 +1,26 @@
+<!-- header.php -->
+
+<?php
+    echo '<div class="sidebar">
+        <img id="closeButton" src="/omiyageEC/src/common/img/close.png">
+
+        <hr>
+        <p>○○○○さん</p>
+        <hr>
+        <p><a href="/omiyageEC/src/G1-3/G1-3-4/index.php">ポイント</a></p>
+        <hr>
+        <p><a href="/omiyageEC/src/G1-3/G1-3-2/index.php">登録情報</a></p>
+        <hr>
+        <p><a href="/omiyageEC/src/G1-3/G1-3-3/index.php">購入履歴</a></p>
+        <hr>
+        <p><a href="/omiyageEC/src/G1-3/G1-3-5/index.php">予約確認</a></p>
+        <hr>
+        <p><a href="">ログイン</a></p>
+        <hr>
+        <p><a href="/omiyageEC/src/common/php/logout.php">ログアウト</a></p>
+        </div>
+        <img id="toggleButton" src="/omiyageEC/src/common/img/user.png" width="40" height="40">
+        <div id="search_all">
+
+        </div>';
+?>
