002226836efc03fd6183cdc8049116ed0defc525
rikuto <rikuriku.shin@gmail.com>
2023-12-01 12:31:38 +0900

    遷移処理の調整

---
+++ src/common/php/cart_check.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // GETリクエストが送信された場合
+    // 接続
+    $pdo = getDatabaseConnection(); 
+
+    // GET受け取り
+    
+
+    // DB追加処理
+
+    // 画面遷移のif判定
+}
+?>
