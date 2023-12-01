c6d52933c415c0cd56dca04e3521f06f682eab01
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-12-01 11:32:39 +0900

    i

--- src/G1-3/G1-3-3/history.php
+++ src/G1-3/G1-3-3/history.php
@@ -1,25 +1,2 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="../../common/css/header.css">
-    <?php require '../..//common/php/DB.php'; ?>
-</head>
-<body>
-    <header>
-        <header>
-            <?php require '../../common/php/header.php'; ?>
-        </header>
+<?php require '../../common/php/DB.php'; ?>
 
-
-        <!-- 詳細表示PHP -->
-        <?php require '../../common/php/detail.php'; ?>
-
-
-            <script src="../../common/js/side_ber.js"></script>
-        
-</body>
-</html>
