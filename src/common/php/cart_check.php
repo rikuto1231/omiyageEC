f942c686aadc9607efbcbde872d9bd119ccab0d9
rikuto <rikuriku.shin@gmail.com>
2023-12-04 10:33:19 +0900

    商品詳細の予約有無の判断処理

--- src/common/php/cart_check.php
+++ src/common/php/cart_check.php
@@ -5,29 +5,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // 接続
     $pdo = getDatabaseConnection(); 
 
-    // GET受け取り
+    // 商品IDの取得
     $merchandise_id = isset($_GET['merchandise_id']) ? $_GET['merchandise_id'] : null;
 
-    // ログインユーザーのIDを取得（仮定）
-    $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
 
-    if ($user_id !== null && $merchandise_id !== null) {
-        // カートに追加する処理
-        $quantity = 1; // 数はhiddで詳細から取得予定
+    // 商品IDが正確に送信されている時
+    if ( $merchandise_id !== null) {
 
+        // DBから商品IDで検索して在庫数を取得。在庫数が０の時に予約含み詳細ページに遷移のif
+        $stmt = $pdo->prepare("SELECT * FROM Merchandise WHERE merchandise_id = :merchandise_id AND stock = :stock");
+        $stmt->bindParam(':merchandise_id', $merchandise_id);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // データベースに追加するSQL
-        $sql = 'INSERT INTO Cart (user_id, merchandise_id, quantity) VALUES (?, ?, ?)';
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$user_id, $merchandise_id, $quantity]);
-
-        // 画面遷移のif判定
-        // 例: カートに追加されたらカートのページにリダイレクト
-        header("Location: /omiyageEC/src/common/php/cart_page.php");
+        header("Location: /omiyageEC/src/G1-6/G1-6-1.php");
         exit();
     } else {
         // ユーザーIDか商品IDが取得できない場合の処理
-        echo 'ユーザーIDまたは商品IDが取得できません。';
+        echo '商品IDが取得できません。';
     }
 }
 
