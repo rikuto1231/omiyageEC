2d79206808cbd0d40d1401c3667f0580ce3e9a85
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-05 13:54:10 +0900

    h

--- src/common/php/reservation.php
+++ src/common/php/reservation.php
@@ -5,12 +5,16 @@
     $sql=$pdo->prepare('select * from Reservation where user_id=?');
     $sql->execute([$id]);
     foreach ($sql as $row) {
+        $number = $row['number'];
+        $date = $row['date'];
         $sql=$pdo->prepare('select * from Merchandise where Merchandise_id=?');
         $sql->execute([$row['merchandise_id']]);
         foreach ($sql as $row) {
             echo'<img src="../../common/img/'.$row['path'].'" width="90px" height="90px" class="img_product">';
             echo'<p>',$row['merchandise_name'],'</p>';
             echo'<p>￥',$row['price'],'</p>';
+            echo'<p>数量：',$number,'</p>';
+            echo'<p>',$date,'</p>';
         }
         echo'<br>';
         echo'<br>';
