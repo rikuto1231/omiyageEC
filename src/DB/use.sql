cc4ca6ed0e8bed4106e0613ef66cbe351b9dfef4
rikuto <rikuriku.shin@gmail.com>
2023-12-05 12:08:35 +0900

    DBの修正

--- src/DB/use.sql
+++ src/DB/use.sql
@@ -54,13 +54,16 @@ CREATE TABLE Point(
     foreign key (user_id) references User(user_id)
 );
 
+
 -- 購入テーブル作成sql
-CREATE TABLE Purchase(
+CREATE TABLE Purchase (
     purchase_id int not null AUTO_INCREMENT,
     user_id int not null,
     cart_id int not null,
     purpose_date DATE not null,
-    primary key (purchase_id)
+    primary key (purchase_id),
+    foreign key (user_id) references User(user_id),
+    foreign key (cart_id) references Cart(cart_id)
 );
 
 -- 購入詳細テーブル作成sql
