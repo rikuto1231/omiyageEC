b7f9d32c9071d54a57ea0f516a5a2191cb458b22
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-05 14:10:07 +0900

    j

--- src/common/php/reservation.php
+++ src/common/php/reservation.php
@@ -2,26 +2,9 @@
     require 'DB.php';
     $pdo = getDatabaseConnection(); 
     // 予約検索のsqlとphp処理
-    $sql=$pdo->prepare('select * from Reservation where user_id=?');
-    $sql->execute([$id]);
-    foreach ($sql as $row) {
-        $number = $row['number'];
-        $date = $row['date'];
-        $sql=$pdo->prepare('select * from Merchandise where Merchandise_id=?');
-        $sql->execute([$row['merchandise_id']]);
-        foreach ($sql as $row) {
-            echo'<img src="../../common/img/'.$row['path'].'" width="90px" height="90px" class="img_product">';
-            echo'<p>',$row['merchandise_name'],'</p>';
-            echo'<p>￥',$row['price'],'</p>';
-            echo'<p>数量：',$number,'</p>';
-            echo'<p>',$date,'</p>';
-        }
-        echo'<br>';
-        echo'<br>';
-    }
     $sql=$pdo->prepare('select*
-    from reservation,Merchandise
-    where  reservation.Merchandise_id=Merchandise.Merchandise_id
+    from Reservation,Merchandise
+    where  Reservation.Merchandise_id=Merchandise.Merchandise_id
     and user_id=?');
     $sql->execute([$id]);
 
@@ -29,6 +12,10 @@
         echo'<img src="../../common/img/'.$row['path'].'" width="90px" height="90px" class="img_product">';
         echo'<p>',$row['merchandise_name'],'</p>';
         echo'<p>￥',$row['price'],'</p>';
+        echo'<p>数量：',$row['number'],'</p>';
+        echo'<p>',$row['date'],'</p>';
+        echo'<br>';
+        echo'<br>';
     }
         
 
