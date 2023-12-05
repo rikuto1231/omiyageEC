adc4b148068e091085a5bb1bff1bfc0006972c87
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-05 14:48:44 +0900

    s

--- src/common/php/reservation.php
+++ src/common/php/reservation.php
@@ -8,15 +8,18 @@
     where  Reservation.Merchandise_id=Merchandise.Merchandise_id
     and user_id=?');
     $sql->execute([$id]);
-
+    echo'<form action="reservation_delete.php" method="POST">';
+    echo '<input type="hidden" name="id" value="',$id,'">';
     foreach($sql  as $row){
         echo'<img src="../../common/img/'.$row['path'].'" class="img_product">';
         echo'<h4 class="a">',$row['merchandise_name'],'</h4>';
         echo'<p class="a">￥',$row['price'],'　　　　数量：',$row['number'],'</p>';
+        echo '<input type="hidden" name="m_id" value="',$row['Merchandise_id'],'">';
         echo'<p><button type="submit" class="delete">取消</button>','　　　　',$row['date'],'</p>';
         echo'<br>';
         echo'<br>';
     }
+    echo'</form>';
         
 
 ?>
