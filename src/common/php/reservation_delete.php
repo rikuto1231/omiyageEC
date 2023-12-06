a0283c48b42769a7928382dccd0cf1869017d08f
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-06 09:27:58 +0900

    f

--- src/common/php/reservation_delete.php
+++ src/common/php/reservation_delete.php
@@ -3,7 +3,9 @@
 require 'DB.php';
     // データベースへの接続を取得
     $pdo = getDatabaseConnection();
-    $sql=$pdo->prepare('delete * from Reservation where user_id=? and merchandise_id=?');
+    $sql=$pdo->prepare('delete * from Reservation where Reservation,Merchandise
+    where  Reservation.Merchandise_id=Merchandise.Merchandise_id 
+    and user_id=? and ');
     $sql->execute([$_POST['id'],$_POST['mid']]);
          ?>
         
