f5afabd6b93fc4cb02ada07cebfe55a452ffe3c4
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-07 14:25:42 +0900

    h

--- src/common/php/personal_update.php
+++ src/common/php/personal_update.php
@@ -2,8 +2,8 @@
     require 'DB.php';
     // データベースへの接続を取得
     $pdo = getDatabaseConnection();
-    $sql=$pdo->prepare('update User set personal_family=?, personal_personal=?, personal_family_kana=?, personal_personal_kana=?, personal_post=?, personal_prefectures=?, personal_city=?, personal_street=?, personal_buliding=?, personal_tel=?, personal_mail_address=? where user_id=?');
-    $sql->execute([$_POST['lastName'],$_POST['firstName'],$_POST['furiganaLastName'],$_POST['furiganaFirstName'],$_POST['postalCode'],$_POST['postalCode'],$_POST['city'],$_POST['street'],$_POST['buliding'],$_POST['phoneNumber'],$_POST['email'],$_SESSION['user_id']]);
+    $sql=$pdo->prepare('update User set name_family=?, name_personal=?, name_family_kana=?, name_personal_kana=?, post_code=?, prefecture=?, city_address=?, street_address=?, buliding=?, tel=?, mail_address=? where user_id=?');
+    $sql->execute([$_POST['lastName'],$_POST['firstName'],$_POST['furiganaLastName'],$_POST['furiganaFirstName'],$_POST['postalCode'],$_POST['postalCode'],$_POST['city'],$_POST['street'],$_POST['building'],$_POST['phoneNumber'],$_POST['email'],$_SESSION['user_id']]);
     header('Location: ../../G1-3/G1-3-2/index.php');
     exit();
 ?>
