8bf8e59dbf0928ed25b3e10efd0d547be58cdf61
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-07 14:47:28 +0900

    b

--- src/common/php/parsonal.php
+++ src/common/php/parsonal.php
@@ -11,6 +11,18 @@ $sql=$pdo->prepare('select * from User where user_id=?');
 echo'<form action="/omiyageEC/src/common/php/personal_update.php" method="POST">';
         // <!-- お名前 -->
         foreach($sql  as $row){
+            $_SESSION['user_id'] = $row['user_id'];
+            $_SESSION['personal_family'] = $row['name_family'];//姓
+            $_SESSION['personal_family_kana'] = $row['name_family_kana'];//姓(カナ)
+            $_SESSION['personal_personal']=$row['name_personal'];//名
+            $_SESSION['personal_personal_kana']=$row['name_personal_kana'];//名(カナ)
+            $_SESSION['personal_post']=$row['post_code'];//郵便番号
+            $_SESSION['personal_prefectures']=$row['prefecture'];//都道府県
+            $_SESSION['personal_city']=$row['city_address'];//市区町村
+            $_SESSION['personal_street']=$row['street_address'];//番地
+            $_SESSION['personal_buliding']=$row['building'];//建物名・部屋番号
+            $_SESSION['personal_tel']=$row['tel'];//電話番号
+            $_SESSION['personal_mail_address']=$row['mail_address'];
         echo '<div class="form-group">';
         echo '<label for="lastName">姓</label>';
                 //    <!-- テキスト位置調整用にid入れてます -->
@@ -61,7 +73,7 @@ echo'<form action="/omiyageEC/src/common/php/personal_update.php" method="POST">
                 // <!-- 住所（建物名・部屋番号） -->
         echo '<div class="form-group">';
         echo '<label for="building">住所（建物名・部屋番号）:</label>';
-        echo '<input type="text" id="building" name="building" class="form-control" value=',$_SESSION['personal_buliding'],'>';
+        echo '<input type="text" id="building" name="buliding" class="form-control" value=',$_SESSION['personal_buliding'],'>';
         echo '</div>';
 
                 // <!-- 電話番号 -->
