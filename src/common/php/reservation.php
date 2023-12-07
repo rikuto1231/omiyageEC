c541e74aa372425c2130bb0cd4c9020b2ca52e94
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-07 12:16:05 +0900

    j

--- src/common/php/reservation.php
+++ src/common/php/reservation.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
     require 'DB.php';
     $pdo = getDatabaseConnection(); 
     // 予約検索のsqlとphp処理
@@ -9,16 +9,16 @@
     and user_id=?');
     $sql->execute([$id]);
     foreach($sql  as $row){
-        echo'<form action="../../common/php/reservation_delete.php" method="POST">';
         echo'<img src="../../common/img/'.$row['path'].'" class="img_product">';
         echo'<h4 class="a">',$row['merchandise_name'],'</h4>';
         echo'<p class="a">￥',$row['price'],'　　　　数量：',$row['number'],'</p>';
+        echo'<form action="../../common/php/reservation_delete.php" method="POST">';
         echo'<input type="hidden" name="mid" value="',$row['merchandise_id'],'">';
         echo'<p><button type="submit" class="delete">取消</button>','　　　　',$row['date'],'</p>';
+        echo'</form>';
         echo'<br>';
         echo'<br>';
     }
-    echo'</form>';
         
 
 ?>
