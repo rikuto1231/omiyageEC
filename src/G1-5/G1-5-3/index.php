36fe22c5285633517565053e18f933cce4cb05bc
rikuto <rikuriku.shin@gmail.com>
2023-12-07 12:07:18 +0900

    読み込み位置変更

--- src/G1-5/G1-5-3/index.php
+++ src/G1-5/G1-5-3/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../../common/css/header.css">
     <?php require '../..//common/php/DB.php'; ?>
@@ -20,7 +21,7 @@
         <?php require '../../common/php/detail.php'; ?>
 
 
-        <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+
         <script src="../../common/js/side_ber.js"></script>
         <script src="../../common/js/button.js"></script>
 
