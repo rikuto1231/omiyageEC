6dd119fa3541060efa1a6b740ace6528491b7c38
rikuto <rikuriku.shin@gmail.com>
2023-12-08 08:11:12 +0900

    修正

--- src/common/php/cart_get.php
+++ src/common/php/cart_get.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 // ユーザIDの確認
 if (isset($_SESSION['user_id'])) {
     // ユーザIDを取得
@@ -10,7 +8,9 @@ if (isset($_SESSION['user_id'])) {
     $pdo = getDatabaseConnection();  
 
     // カートの中にユーザの商品が入っているかチェック
-    $checkCartQuery = "SELECT * FROM Cart WHERE user_id = :user_id AND purchased = 0";
+    $checkCartQuery = "SELECT c.*, m.merchandise_name, m.path, m.price, m.stock FROM Cart c
+                        JOIN Merchandise m ON c.merchandise_id = m.merchandise_id
+                        WHERE c.user_id = :user_id AND c.purchased = 0";
 
     $checkCartStmt = $pdo->prepare($checkCartQuery);
     $checkCartStmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
@@ -26,9 +26,10 @@ if (isset($_SESSION['user_id'])) {
             echo '<p id="cart_mess">カートに入っているアイテム</p>';
             echo '<img class="img_product" src="/omiyageEC/src/common/img/cart.jpg" width="100" height="100">';
             echo '<div class="in_cart">';
-            echo '<h5>商品名: ' . $product['name'] . '</h5>';
+            echo '<h5>商品名: ' . $product['merchandise_name'] . '</h5>';
+            echo '<h5>価格: ¥' . number_format($product['price']) . '</h5>';
             echo '<h5><select name="number">';
-            for ($i = 1; $i <= 10; $i++) {
+            for ($i = 1; $i <= $product['stock']; $i++) {
                 echo '<option value="' . $i . '">' . $i . '</option>';
             }
             echo '</select></h5>';
@@ -36,9 +37,8 @@ if (isset($_SESSION['user_id'])) {
             echo '</div>';
             echo '</div>';
         }
-
     } else {
-    echo "A";
+        echo "A";
     }
 } else {
     // ユーザIDがセッションに保存されていない場合（未ログインなど）
