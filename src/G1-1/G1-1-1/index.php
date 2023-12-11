47fc93b250b08743a64cedc3cf70d3d1705e7283
2201150 <147681633+2201150@users.noreply.github.com>
2023-12-11 12:52:18 +0900

    更新

---
+++ src/G1-1/G1-1-1/index.php
@@ -0,0 +1,28 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/G1-1-1.css"/>
+    <title>Document</title>
+</head>
+<body>
+
+    
+    <h1>ログイン</h1>
+    <a><p>会員の方はこちらから<br></a>
+    <a href="../G1-1-2/index.php" class="btn">ログイン</a>
+
+
+
+
+    <img src="/omiyageEC/src/common/img/mement.png" alt="めめんと"></img>
+    <a><p>初めての方はこちらから<br></a>
+        
+        <a href="../G1-1-3/index.php" class="btn2">新規会員登録</a>
+    </p>
+
+</body>
+</html>
---
+++ src/G1-1/G1-1-2/index.php
@@ -0,0 +1,34 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/G1-1-2.css"/>
+    <title>会員ログイン</title>
+</head>
+<body>
+    <h1>会員ログイン</h1>
+    
+    <form  action="../../common/php/login.php"  method="post">
+        <p>
+            <label for="mail">メールアドレス</label><br>
+            <div class="box1">
+                <input type="text" id="mail" name="mail" class="m" placeholder="入力してください" required>
+            </div>
+        </p>
+        <p>
+            <label for="pass">パスワード</label><br>
+            <div class="box2">
+                <input type="password" id="pass" name="pass" class="p" placeholder="パスワードを入力" required>
+            </div>
+        </p>
+
+        <button type="submit" class="btn">ログイン</button>
+        
+    </form>
+
+    <button class="btn2" onclick="location.href='../G1-1-1/index.php'">戻る</button>
+</body>
+</html>
