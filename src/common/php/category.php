ca4db692fe8e35a297faf94a68ed0e16ca020ac7
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-12-12 13:54:39 +0900

    kk

--- src/common/php/category.php
+++ src/common/php/category.php
@@ -5,15 +5,14 @@ require '../../common/php/DB.php';
 $pdo = getDatabaseConnection();
 $sql=$pdo->prepare('select DISTINCT category from Merchandise ');
 
-$result = $dbConnection->query($sql);
 
-$dbConnection->close();
     
 
-    if ($result->num_rows > 0) {
+    if ($sql > 0) {
         while($row = $result->fetch_assoc()) {
             echo "<option value='". $row['category'] ."'>". $row['category'] ."</option>";
         }
     }
 
 ?>
+
