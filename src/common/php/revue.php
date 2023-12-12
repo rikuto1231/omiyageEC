5632eb83b870a9a31806ca28e207925420cb9000
satoi0127 <148302675+satoi0127@users.noreply.github.com>
2023-12-12 12:13:21 +0900

    h

--- src/common/php/revue.php
+++ src/common/php/revue.php
@@ -1,19 +1,50 @@
+<?php 
+ require 'DB.php';
 
-   <div id="revew">
-    <h2><img src="/omiyageEC/src/common/img/user.png" alt="代替テキスト"width="7%" height="14%">
-    あいうえお
-    </h2>
-    <h2>m/d</h2>
-    <h2><span class="star">★</span>☆☆☆☆</h2>
-    <h2>とてもいい</h2>
-    <div id="center">
-    <div id="fonts">×月×日に購入<br>~~~~~~~~~~<br>~~~~~~~~~~<br>~~~~~~~~~~</div>
-    </div>
-    </div>
-    <div id="center">
-        <button class="input" onclick="location.href='../../G1-7/G1-7-2/index.php'">レビュー入力</button>
-    </div>
-    <br>
-    <div id="center">
-        <button class="back"  onclick="location.href='../../G1-5/G1-5-2/index.php'">もどる</button>
-    </div>
+
+    $pdo = getDatabaseConnection(); 
+    // ポイント検索のsqlとphp処理
+    $sql=$pdo->prepare('select*
+    from Review,User
+    where  User.User_id=Review.User_id
+    and Merchandise_id=?');
+    $sql->execute([$id]);
+    foreach($sql  as $row){
+   echo'<div id="revew">';
+   echo'<h2><img src="/omiyageEC/src/common/img/user.png" alt="代替テキスト"width="7%" height="14%">';
+   echo$row['name_personal'];
+   echo' </h2>';
+   echo' <h2>',$row['post'],'</h2>';
+   echo' <h2><span class="star">';
+   switch ($row['review_num']){
+    case '1':
+        echo'★';
+      break;
+    case '2':
+        echo'★★';
+      break;
+    case '3':
+        echo'★★★';
+      break;
+    case '4':
+        echo'★★★★';
+      break;
+    case '5':
+        echo'★★★★★';
+      break;
+    }
+   echo'</span></h2>';
+   echo' <h2>',$row['review_title'],'</h2>';
+   echo' <div id="center">';
+   echo' <div id="fonts">',$row['review_text'],'</div>';
+   echo' </div>';
+   echo' </div>';
+    }
+   echo' <div id="center">';
+   echo'<button class="input" onclick="',"location.href='../../G1-7/G1-7-2/index.php",'">レビュー入力</button>';
+   echo' </div>';
+   echo' <br>';
+   echo' <div id="center">';
+   echo'     <button class="back"  onclick="',"location.href='../../G1-5/G1-5-2/index.php",'">もどる</button>';
+   echo' </div>';
+    ?>
