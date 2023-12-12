5bbf52143d1e384bb3686e65760f42ef543ac276
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2023-12-12 12:38:43 +0900

    j

--- src/common/php/category.php
+++ src/common/php/category.php
@@ -5,8 +5,8 @@ require '../../common/php/DB.php';
 $pdo = getDatabaseConnection();
 $sql=$pdo->prepare('select * from Merchandise where category');
 
-    $sql=$pdo->prepare('');
-    $sql->execute([$id]);
+    $sql=$pdo->prepare('merchandise_id');
+    
 
     if ($sql->num_rows > 0) {
         while($row = $sql->fetch_assoc()) {
