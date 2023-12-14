17181fa010976c12898c9543a1af7d5b770faa79
rikuto <rikuriku.shin@gmail.com>
2023-12-14 10:18:19 +0900

    調整

--- src/common/php/purchase.php
+++ src/common/php/purchase.php
@@ -13,38 +13,29 @@ try {
     if (!empty($cart_ids)) {
         $purchaseDate = date("Y-m-d");
 
-        foreach ($cart_ids as $cart_id) {
-            // 購入がすでに行われているか確認
-            $checkPurchaseQuery = "SELECT purchase_id FROM Purchase WHERE user_id = :user_id AND cart_id = :cart_id";
-            $checkPurchaseStmt = $pdo->prepare($checkPurchaseQuery);
-            $checkPurchaseStmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-            $checkPurchaseStmt->bindParam(':cart_id', $cart_id, PDO::PARAM_INT);
-            $checkPurchaseStmt->execute();
+        // 購入がまだ行われていない場合のみ購入
+        if (empty(checkExistingPurchase($pdo, $user_id, $cart_ids))) {
+            $purchase_id = insertPurchase($pdo, $user_id, $cart_ids[0], $purchaseDate);
 
-            if ($checkPurchaseStmt->rowCount() == 0) {
-                // 購入がまだ行われていない場合のみ購入を行う
-                $purchase_id = insertPurchase($pdo, $user_id, $cart_id, $purchaseDate);
+            // 購入詳細情報をPurchaseDetテーブルに挿入
+            $checkCartQuery = "SELECT merchandise_id, quantity FROM Cart WHERE user_id = :user_id AND cart_id = :cart_id";
+            $checkCartStmt = $pdo->prepare($checkCartQuery);
+            $checkCartStmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $checkCartStmt->bindParam(':cart_id', $cart_ids[0], PDO::PARAM_INT);
+            $checkCartStmt->execute();
 
-                // 購入詳細情報をPurchaseDetテーブルに挿入
-                $checkCartQuery = "SELECT merchandise_id, quantity FROM Cart WHERE user_id = :user_id AND cart_id = :cart_id";
-                $checkCartStmt = $pdo->prepare($checkCartQuery);
-                $checkCartStmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-                $checkCartStmt->bindParam(':cart_id', $cart_id, PDO::PARAM_INT);
-                $checkCartStmt->execute();
+            $products = $checkCartStmt->fetchAll(PDO::FETCH_ASSOC);
 
-                $products = $checkCartStmt->fetchAll(PDO::FETCH_ASSOC);
-
-                foreach ($products as $product) {
-                    insertPurchaseDetail($pdo, $purchase_id, $product['merchandise_id'], $product['quantity']);
-                }
-
-                // ポイント情報を挿入
-                $point = $_SESSION['points'];
-                insertPoint($pdo, $user_id, $point);
-
-                // カートから商品を購入済み
-                CartAsPurchased($pdo, $cart_id);
+            foreach ($products as $product) {
+                insertPurchaseDetail($pdo, $purchase_id, $product['merchandise_id'], $product['quantity']);
             }
+
+            // ポイント情報を挿入
+            $point = $_SESSION['points'];
+            insertPoint($pdo, $user_id, $point);
+
+            // カートから商品を購入済み
+            CartAsPurchased($pdo, $cart_ids[0]);
         }
     }
 
@@ -54,6 +45,15 @@ try {
 
 } catch (PDOException $e) {
     echo "エラー: " . $e->getMessage();
+}
+
+//最終調整で追加したcheck関数
+function checkExistingPurchase($pdo, $user_id, $cart_ids) {
+    $checkPurchaseQuery = "SELECT purchase_id FROM Purchase WHERE user_id = :user_id AND cart_id IN (" . implode(",", $cart_ids) . ")";
+    $checkPurchaseStmt = $pdo->prepare($checkPurchaseQuery);
+    $checkPurchaseStmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $checkPurchaseStmt->execute();
+    return $checkPurchaseStmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 
